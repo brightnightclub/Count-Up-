9660834d05014db78bc6ef3d3d8b14bf8eadd991
= <student@epicodus.com>
2015-08-17 21:26:13 -0700

    brightnight truly

--- lizt/goalz_go.rb
+++ lizt/goalz_go.rb
@@ -1,9 +1,29 @@
 require_relative 'classes'
 
-def add_one(number)
-  print "(#{number}/#{number}) + "
+def add_one(number, color_code)
+  print "\e[#{color_code}m (#{number}/#{number})  \e[0m + "
   number/number
 end
+
+def color_code
+ [31,32,33,34,35,36].sample
+end
+
+#
+# def color(index)
+#   normal = "\e[#{index}m#{index}\e[0m"
+#   bold = "\e[#{index}m\e[1m#{index}\e[0m"
+#   "#{normal}  #{bold}  "
+# end
+#
+# 8.times do|index|
+#   line = color(index + 1)
+#   line += color(index + 30)
+#   line += color(index + 90)
+#   line += color(index + 40)
+#   line += color(index + 100)
+#   puts line
+# end
 
 goalz = Goalz.get_number
 
@@ -19,7 +39,7 @@ array.delete(0)
 num = array[0]
 climber = 0
 until climber == goalz.goalz - 1
-  climber += add_one(num)
+  climber += add_one(num, color_code)
   num = array.shuffle[0]
 end
 print "(#{num}/#{num}) = #{goalz.goalz}"
